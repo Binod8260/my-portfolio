b7453b4ff944e4e72007f46e9f7c3cce88612f8d
Binod8260 <binoddas1143@gmail.com>
2026-07-29 21:32:18 +0530

    initial portfolio website

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './navbar/navbar';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Skills } from './skills/skills';
+import { Projects } from './projects/projects';
+import { Certificate } from './certificate/certificate';
+import { Resume } from './resume/resume';
+import { Contact } from './contact/contact';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+  Navbar,
+  Home,
+  About,
+  Skills,
+  Projects,
+  Certificate,
+  Resume,
+  Contact,
+  Footer
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-portfolio');
+
+  scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
 }
+}
+
+
