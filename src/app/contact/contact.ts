8a980c776bb6fcffa7d0b4fbbbd801ee16a51f68
Binod8260 <binoddas1143@gmail.com>
2026-07-31 15:26:51 +0530

    updated navbar

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.css',
+  styleUrl: './contact.css'
 })
-export class Contact {}
+export class Contact {
+
+  name = '';
+  email = '';
+  message = '';
+
+  sendEmail() {
+
+    const templateParams = {
+      name: this.name,
+      email: this.email,
+      message: this.message
+    };
+
+    emailjs.send(
+      'service_4fuqzfz',
+      'template_c3i232w',
+      templateParams,
+      'T68B_Stnb4iEXcVws'
+    ).then(() => {
+      alert('Message Sent Successfully!');
+      this.name = '';
+      this.email = '';
+      this.message = '';
+    }).catch((error) => {
+  console.log(error);
+  alert(error.text ||JSON.stringify(error));
+});
+    
+  }
+}
--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+}
