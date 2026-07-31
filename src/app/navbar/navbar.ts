29ae2fa3802f6cdd95debfba193fc9fff03acab6
Binod8260 <binoddas1143@gmail.com>
2026-07-31 16:40:08 +0530

    update

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
